f6ab253ab93213660e88c9562239a74a77b132a5
alexiglnt <alexigallonet@gmail.com>
2024-04-20 18:34:10 +0200

    Fix Skill Icons on Mobile

--- src/components/Skills/Techstack.js
+++ src/components/Skills/Techstack.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
 import jsonSkills from "../../jsons/skills.json"
 
 function Techstack() {
@@ -10,16 +10,16 @@ function Techstack() {
           return (
             <>
               <h3 style={{textAlign: "left", marginLeft: "15px", marginTop: "100px"}} > {skill.name} </h3>
-              <Row>
+              <Container className="inline-flex-wrap" >
                 {skill.skills.map((skill) => (
-                  <Col xs={4} md={2} key={skill.name} className="tech-icon-wrapper">
+                  <Col xs={6} md={2} key={skill.name} className="tech-icon-wrapper">
                     <a href={skill.link} target="_blank" rel="noreferrer" className="tech-icons">
                       <img src={skill.logo} alt={skill.name} />
                       <p style={{ color: "black", fontSize: "20px", marginTop: "20px", marginBottom: "0px" }}>{skill.name}</p>
                     </a>
                   </Col>
                 ))}
-              </Row> 
+              </Container> 
             </>
           )
         })
